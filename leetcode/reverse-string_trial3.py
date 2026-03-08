710a58eb0917e6947fdc8466cf084f8cf8279a92
Jenber L. <jenberligab@gmail.com>
2026-03-08 13:39:32 +0300

    Add solution for Reverse String (trial 3)

---
+++ leetcode/reverse-string_trial3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start = 0
+        end = len(s) -1 
+        for i in range(len(s)):            
+            if start > end:
+                break
+            s[start],s[end] = s[end],s[start]
+            end -= 1
+            start += 1
+        return s
