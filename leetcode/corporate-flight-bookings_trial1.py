d6d8343a0ceafba748cd3b8881780ff0a0e1220c
Jenber L. <jenberligab@gmail.com>
2026-03-27 22:17:11 +0300

    Add solution for Corporate Flight Bookings (trial 1)

---
+++ leetcode/corporate-flight-bookings_trial1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        arr = [0] * n    
+        for first, last, seats in bookings:
+            arr[first - 1] += seats
+            if last < n:
+                arr[last] -= seats
+        for i in range(1, n):
+            arr[i] += arr[i - 1]
+
+        return arr
+        
